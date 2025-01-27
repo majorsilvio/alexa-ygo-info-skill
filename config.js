601984b5df574f980c513a0057b139604ed3e24d
Silvio Paiva <42873611+majorsilvio@users.noreply.github.com>
2025-01-27 19:51:57 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    HELP_MESSAGE : 'Você pode perguntar por informações sobre uma carta. Por exemplo, quais os detalhes da carta ?',
+    HELP_REPROMPT : 'Como posso ajudar?',
+    STOP_MESSAGE : 'Até a proxima!',
+    URL: "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+}
+module.exports = config;
